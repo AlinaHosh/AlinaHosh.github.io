f3897d0cbd92b07361f371c765e918ddb0d3bd14
AlinaHosh <73576527+AlinaHosh@users.noreply.github.com>
2021-12-08 15:17:01 +0200

    Add files via upload

---
+++ levelEnglish/js1.js
@@ -0,0 +1,20 @@
+
+$(':button').css({
+  color:"rgb(171, 171, 201)",   
+  border: "4px solid rgb(171, 171, 201)",
+  fontSize: "30px",
+  textAlign: "center",
+  fontWeight: "bolder",
+  borderRadius: "150px",
+width: "200px",
+height: "200px",
+margin: "auto",
+marginTop: "20px"
+});
+$('.but').css({
+  display:"flex",
+    flexDirection:"row",
+    justifyContent: "space-around",
+    alignSelf: "center",
+    width:"50%"
+});
